ce6b860c89157703f49e4bdd62de2fa3dd995c58
Leo Fernandez <130509266+leofernandez3@users.noreply.github.com>
2026-03-15 02:23:51 +0100

    Delete Week_6_Assignments/NodeJS/Assignment-4-The-timeline-API directory

--- Week_6_Assignments/NodeJS/Assignment-4-The-timeline-API/Config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(
-  "mongodb+srv://leofernandez87_db_user:TI8Zjc1LNnYcCS2q@cluster0.ogxtf90.mongodb.net/assignment4",
-)
-.then(() => console.log("DB connected successfully"))
-.catch(err => console.log("DB connection error:", err));
